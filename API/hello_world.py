12effb6ba8af4725ca8b0915cbbe7ef3d88eaf29
lofi.eth <134239349+lofieth@users.noreply.github.com>
2024-07-17 16:18:10 +0200

    update hello_world.py and app.py

--- API/hello_world.py
+++ API/hello_world.py
@@ -1,6 +1,15 @@
+from fastapi.responses import JSONResponse
+from fastapi import HTTPException, status
 from database.database import database
 
 
 def hello_world(text):
-    hello_world = database("hello_world " + text)
-    return hello_world
+    try:
+        hello_world = database("hello_world " + text)
+        return JSONResponse({"index": hello_world})
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e),
+        )
--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import asyncio
 
-from fastapi import FastAPI
-from function.body import Body
-from function.example import example
 from API.hello_world import hello_world
+from function.example import example
+from function.body import Body
+from fastapi import FastAPI
 
 
-app = FastAPI(docs_url="/docs", redoc_url="/redocs")
+app = FastAPI(
+    docs_url="/docs",
+    redoc_url="/redocs",
+)
 
 
 @app.post(
